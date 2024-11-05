2e5b7170fcebdabb7772bb36548e05158e67b156
Palcroos <buffalorojo@gmail.com>
2024-11-05 07:43:18 -0600

    Commit3_cambioJSyHTML

--- javas.js
+++ javas.js
@@ -1,5 +1,23 @@
-const boton = document.getElementById("siete");  // Obtener el botón con id "siete"
+const botones = document.querySelectorAll(".boton");
+botones.forEach(boton => {
 boton.addEventListener("click", function() {
-    const celda = document.getElementById("celda");  // Obtener la celda con id "celda"
-    celda.innerHTML = 'Hola';  // Cambiar el contenido de esa celda a "Hola"
-});
+    const num = this.dataset.num;
+    const celda = document.getElementById("celda");
+    celda.innerHTML += num;
+})});
+
+const operadores = document.querySelectorAll(".operador");
+operadores.forEach(operador => {
+operador.addEventListener("click", function() {
+    const op = this.dataset.op;
+    const celda = document.getElementById("celda");
+    celda.innerHTML += op;
+})});
+
+const borra = document.querySelectorAll(".borrar");
+borra.forEach(borrar => {
+operador.addEventListener("click", function() {
+    const op = this.dataset.op;
+    const celda = document.getElementById("celda");
+    celda.innerHTML += op;
+})});
